7ae49cff7003dc9b75ebf102de03b8c392996d61
Dong-Es <qufqlc164@gmail.com>
2024-06-12 14:16:50 +0900

    asd

--- src/dev/App.tsx
+++ src/dev/App.tsx
@@ -1,24 +1,8 @@
-/********************************************************************************
- * Copyright (c) 2024 Contributors to the Eclipse Foundation
- *
- * See the NOTICE file(s) distributed with this work for additional
- * information regarding copyright ownership.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Apache License, Version 2.0 which is available at
- * https://www.apache.org/licenses/LICENSE-2.0.
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- *
- * SPDX-License-Identifier: Apache-2.0
- ********************************************************************************/
 
 import { useState } from 'react'
-import { Button, Cards, ViewSelector } from '../main'
+import { Button } from '../components/basic/Button/Button'
+import { Cards } from '../components/basic/Card'
+
 
 const isPrime = (num: number) => {
   for (let i = 2, s = Math.sqrt(num); i <= s; i++)
@@ -62,7 +46,7 @@ const cardsArgs = {
 
 export default function App() {
   const [count, setCount] = useState<number>(1)
-  const [view, setView] = useState<ViewType>(ViewType.ALL)
+  const [view,] = useState<ViewType>(ViewType.ALL)
 
   return (
     <main style={{ padding: 80 }}>
@@ -74,17 +58,6 @@ export default function App() {
       >
         count is {count}
       </Button>
-      <ViewSelector
-        views={Object.keys(ViewType).map((buttonText) => ({
-          buttonText,
-          buttonValue: buttonText,
-          onButtonClick: (e: React.MouseEvent) => {
-            const target = e.target as HTMLButtonElement
-            setView(target.value as ViewType)
-          },
-        }))}
-        activeView={view}
-      />
       <div style={{ height: 16 }} />
       <Cards
         {...cardsArgs}
